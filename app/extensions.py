f941d414ef03bd32af9951e37cedf9d79afecd1a
Miqueas Correa <miki.mate.tizi@gmail.com>
2025-12-12 22:25:51 -0300

    agregor archivo extensions

---
+++ app/extensions.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+"""
+Este módulo inicializa la extensión SQLAlchemy para su uso con Flask.
+Atributos:
+    db (SQLAlchemy): Instancia de SQLAlchemy utilizada para interactuar con la base de datos.
+Uso:
+    Importe 'db' desde este módulo para definir modelos y realizar operaciones de base de datos en la aplicación Flask.
+"""
+
+db = SQLAlchemy()
+jwt = JWTManager()
